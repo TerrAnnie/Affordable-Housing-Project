26292199b568af4e4a99b80082738ac69dc085c5
TerrAnnie <60158520+TerrAnnie@users.noreply.github.com>
2021-11-11 11:08:24 -0600

    Edited Client class to output to file, Added file
    Next will need to do string formatting to make things look better and more legible

--- Client Class For Program.py
+++ Client Class For Program.py
@@ -5,17 +5,20 @@
 #Class for a possible client using the software
 #Code by TerrAnnie Scott
 #closefile in developer
+#Implement printing basic info to file
 
 class Client():#Person class asks persons name, address, age and maritial status.Maybe rename to client 
     def __init__(self,firstname= "N/A", lastname= "N/A", age=0, city = "N/A", state ="N/A", Zip= 00000, StreetAddress = "N/A", Target_Zip=00000):
         self.firstname = firstname
-        self.age= age
+        self.age = age
         self.lastname= lastname
         self.city = city
         self.state = state
         self.Zip= Zip
         self.StreetAddress = StreetAddress
         self.Target_Zip = Target_Zip
+        self.PrintList =[]
+        self.InOppurtunityZone = False
       
     def getname (self):
         firstname = self.firstname
@@ -25,43 +28,54 @@ class Client():#Person class asks persons name, address, age and maritial status
     def getage(self):
         return self.age
     def getAddress(self):
-        Address = self.StreetAddress + " " +  self.city + " " + self.state + " " + self.Zip
+        Zipcode = str(self.Zip)
+        Address = self.StreetAddress + " " +  self.city + " " + self.state + " " + Zipcode
         return Address
     def GetMarried(self):
         return self.Ismarried
-    def PrintAllInfo (self):
-        print("Here is the information we have on file.")
-        print()
-        print(self.getname())
-        print(self.getage())
-        print(self.getAddress())
-        if(self.GetMarried()):
-            print("Marriage Status = Yes")
-        else:
-            print("Marriage Status = NO")
+  
+        
+    
     def OppurtunityZoneCheck(self):
-        InOppurtunityZone = False
+      
         string = "N/A"
         ZipCode = "N/A"
         with open("Oppurtunity Zone Zipcodes.txt", "r") as open_zipcodes:
             for line in open_zipcodes:
                 if '*' in line:
-                    Zipcode = int(line[0:5])
-                    string= "This Zipcode may be in an oppurtunity Zone"
+                    Zipcode = int(line[0:5])#slice the line check. to only obtain zipcode
+                    self.PrintList.append("This Zipcode may be in an oppurtunity zone. You may be eligible to receive preferential tax treatment from the IRS.")
                 else:
                     Zipcode = int(line)
-                    string = "This Zipcode is an oppurtunity zone"
+                    self.PrintList.append("This Zipcode is in an oppurtunity zone. You may be eligible to receive preferential tax treatment from the IRS.")
 
                 if Zipcode == self.Target_Zip:
-                    InOppurtunityZone = True
+                    self.InOppurtunityZone = True
                     break
         open_zipcodes.close()
-        if InOppurtunityZone == True:
-            print(string)
-
-C1 = Client("TerrAnnie","Scott","21","KC","MO",64138, "blah", 64116)
-C1.OppurtunityZoneCheck()
+      
             
+    def PrintAllInfo (self):
+        self.OppurtunityZoneCheck()
+        file_open = open("Client Property Facts.txt", "w")
+        file_open.write("Here's all the information we have on file:")
+        file_open.write ("\n")
+        file_open.write ("Name:  %s" % (self.getname()))
+        file_open.write ("\n")
+        file_open.write ("Age:  %s" % (self.age))
+        file_open.write ("\n")
+        file_open.write ("Address:  %s" % (self.getAddress()))
+        file_open.write ("\n")
+        file_open.write ("Target Property Zipcode:  %d" % (self.Target_Zip))
+        file_open.write ("\n")
+        if self.InOppurtunityZone == True:
+             file_open.write(self.PrintList[1])#Do make a loop here later
+        file_open.close()
+
+        
+C1 = Client("TerrAnnie","Scott","21","KC","MO",64138, "blah", 64116)
+C1.PrintAllInfo()
+
 
 
 
