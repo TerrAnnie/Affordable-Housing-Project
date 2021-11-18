f3bacc6901c82a5af2863e80bc9786fa24dffd40
TerrAnnie <60158520+TerrAnnie@users.noreply.github.com>
2021-11-18 10:21:21 -0600

    Testing Mostly

--- Developer.py
+++ Developer.py
@@ -7,6 +7,9 @@
 #Option for senior living
 #Cateogory 
 #Is the land one small lot (1 acre or less), one large lot (more than 1 acre), contiguous lots, multiple lots, or unknown?
+#To Do: File writing, String formatting,Testing
+#Check if file already exists if so we can collect already inputed information
+#Create a file with Common Defenitions and Links 
 
 
 class Developer():
@@ -18,6 +21,7 @@ class Developer():
        self.Category = "N/A"
        self.Develop = "N/A"
        self.LotType = "N/A"
+       self.DevelopStr = "N/A"
     def getInvestment(self):
         return self.InvestmentRange
     def getDevelopmentType(self):
@@ -33,20 +37,25 @@ class Developer():
                 return "Condominium Building"
         if TypeofBuild == 4:
                 return "Clustered Development"
-    def LotType(self):
+    def SetLotType(self):
         LandSize = self.LandSize.upper()#Take care of casing
-        if self.LandSize == "ONE SMALL LOT" or self.LandSize =="MULTIPLE LOTS":
+        if LandSize == "ONE SMALL LOT" or LandSize =="MULTIPLE LOTS":
             self.LotType = "Multiple/one small"
-        elif self.LandSize == "CONTIGUOUS" or self.LandSize =="ONE LARGE LOT":
+        elif LandSize == "CONTIGUOUS" or LandSize =="ONE LARGE LOT":
             self.LotType = "Contiguous"
         else:
             self.LotType = "Inquire with the KC Land Bank to find developable land. https://www.kcmolandbank.org/"
             
     def IsZonable(self):
         if self.ZonedFor == "R-80":
+            self.Develop = "Parcel may not be developable. Check for variances in zoning of this area."
             return False
+        else:
+            return True
             
-            
+    def getDevelop(self):
+        return self.Develop
+        
         
     def InvestmentRangeFacts(self):
         IR = self.InvestmentRange
@@ -57,19 +66,25 @@ class Developer():
         else:
             self.Category = "HIGH"
     def DevelopmentFacts(self):
-        
-        if self.IsZonable == True:
+        self.InvestmentRangeFacts()
+        print (self.IsZonable())
+        self.SetLotType()##Set the lot type
+        if self.IsZonable() == True:
             if self.DevelopmentType == True:
+                print(self.Category)
                 if self.Category == "LOW" and self.LotType == "Multiple/one small":
+                    
                     if self.ZonedFor == "R-10" or self.ZonedFor == "R-5" or self.ZonedFor == "R-2.5":
                         self.Develop =  self.BuildType(1)
                 elif self.Category == "MEDIUM" and self.LotType == "Contiguous":
-                    if self.ZonedFor == "R-0.5" or self.ZonedFor == "R-1.5" or self.ZonedFor == "R-2.5" or self.ZonedFor == "R-0.3":
-                        self.Develop = self.BuildType (4)
+                        print(100)
+                        if self.ZonedFor == "R-0.5" or self.ZonedFor == "R-1.5" or self.ZonedFor == "R-2.5" or self.ZonedFor == "R-0.3":
+                            self.Develop = self.BuildType (4)
                 elif self.Category == "HIGH" and self.LotType == "Contiguous":
-                    if self.ZonedFor == "R-0.5" or self.ZonedFor == "R-1.5" or self.ZonedFor == "R-0.3":
-                        self.Develop = BuildType (2)
-            else:
+                        if self.ZonedFor == "R-0.5" or self.ZonedFor == "R-1.5" or self.ZonedFor == "R-0.3":
+                            self.Develop = BuildType (2)
+
+        else:
                 if self.DevelopmentType == True:
                     if self.Category == "LOW" and self.LotType == "Multiple/one small":
                         if self.ZonedFor == "R-10" or self.ZonedFor == "R-5" or self.ZonedFor == "R-2.5":
@@ -81,25 +96,27 @@ class Developer():
                         if self.ZonedFor == "R-0.5" or self.ZonedFor == "R-1.5" or self.ZonedFor == "R-0.3":
                             self.Develop = BuildType (3)
             
-       
         
-    def PrintToFile(self):
-        self.InvestmentRangeFacts()
+    def PrintToFile(self):#Add remaning text Zoned For and Lot Type
         self.DevelopmentFacts()
         file_open= open("Property Facts.txt","w")
         file_open.write(("Your investment is $%d." % (self.getInvestment())))
         file_open.write("\n \n")
-        if self.DevelopmentType == True:
-            file_open.write(("Because your development is intended to be low-income and categorized as %s we reccomend this type of building: %s" % (self.Category, self.Develop)))
+        if self.DevelopmentType == True:#If its low income
+            print(self.LotType)
+            
+            file_open.write("Because your development is intended to be low-income,categorized as %s, is zoned for %s, and the lot type is %s we reccomend this type of building: %s" % (self.Category,self.ZonedFor, self.LotType,self.Develop))
+
         else:
-            file_open.write(("Because your development is intended to be mixed-income and categorized as %s we reccomend this type of building: %s" % (self.Category, self.Develop)))
+            file_open.write("Because your development is intended to be low-income,categorized as %s, is zoned for %s, and the lot type is %s we reccomend this type of building: %s" % (self.Category, self.Develop,self.ZonedFor, self.LotType))
+
 
         file_open.close()
             
                     
                     
                 
-D1= Developer(4000000, True)
+D1= Developer(4000000, True, "Contiguous", "R-2.5")
 D1.PrintToFile()
 
 
